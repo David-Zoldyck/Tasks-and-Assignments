1115be998fd86eb0ba70453401f3e88cb895fe8e
David-Zoldyck <116853256+David-Zoldyck@users.noreply.github.com>
2022-11-25 10:35:22 +0100

    Add files via upload

---
+++ DAUDA'S SHOE WEBSITE/index.js
@@ -0,0 +1,210 @@
+//populated cart array for puppies
+var items = [
+   {
+      id: 1,
+      name: "Nike Track Racers",
+      image: "images/image_1.png",  
+      price: 15000,
+   },
+   {
+      id: 2,
+      name: "Nike Basketball Trainers",
+      image: "images/image_2.png",
+      price: 17000,
+   },
+   {
+      id: 3,
+      name: "Nike Kyries",
+      image: "images/image_3.png",
+      price: 25000,
+   },
+   {
+      id: 4,
+      name: "Clarks Sneakers",
+      image: "images/image_4.png",
+      price: 10000,
+   },
+   {
+      id: 5,
+      name: "Oakland Boots",
+      image: "images/image_5.png",
+      
+      price: 14000,
+   },
+   {
+      id: 6,
+      name: "Fendi Flys 2",
+      image: "images/image_6.png",
+      price: 22000,
+   },
+   {
+      id: 7,
+      name: "Nike Curry Series",
+      image: "images/image_7.png",
+      price: 17000,
+   },
+   {
+      id: 8,
+      name: "Bootleg Timberland",
+      image: "images/image_8.png",
+      price: 14000,
+   },
+   {
+      id: 9,
+      name: "Nike Airforce",
+      image: "images/image_9.png",
+      
+      price: 20000,
+   },
+   {
+      id: 10,
+      name: "Drill Sneakers",
+      image: "images/image_10.png",
+      price: 25000,
+   },
+   {
+      id: 11,
+      name: "Senji Loafers",
+      image: "images/image_11.png",
+      price: 27000,
+   },
+   {
+      id: 12,
+      name: "All Stars Sneakers",
+      image: "images/image_12.png",
+      price: 18000,
+   },
+
+];
+
+var cart = [];
+
+//Home page
+$(document).ready(function () {
+   var cartHTML = $.map(items, function (item) {
+      return (
+         '<div class="item">' +
+         `<img src=${item.image} class="item-image"> ` +
+         "</img>" +
+         "<h4>" +
+         item.name +
+         "</h4>" +
+         `<div class="item-details">` +
+         "<span>" +
+         ` ₦ ${item.price}` +
+         "</span>" +
+         "<button class=addCart>" +
+         "Add to Cart" +
+         "</button>" +
+         "</div>" +
+         "</div>"
+      );
+   });
+   $("#cart").html(cartHTML);
+});
+
+//because button is dynamically created, we need to use event delegation
+const cartArray = [];
+$(document).on("click", ".addCart", function () {
+   var name = $(this).parent().parent().children("h4").text();
+   console.log(name)
+   var price = $(this).parent().children("span").text();
+   var image = $(this).parent().parent().children("img").attr("src");
+
+   const cart = new Object();
+   cart.name = name;
+   cart.price = price;
+   cart.image = image;
+   cart.quantity = 1;
+
+   const result = cartArray.findIndex((item) => name == item.name);
+   console.log(result);
+   if (result != -1) {
+    alert("Item already in cart"); 
+    return false;
+   }
+   cartArray.push(cart);
+
+   //store cart in local storage
+   
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+
+   alert("Successfully added to cart")
+  
+});
+
+//increment quantity in localstorage
+$(document).on("click", ".incrementCart", function () {
+    const name = $(this).parent().parent().parent().children(".first_row").children("h4").text();
+    const quantity = $(this).parent().children("span");
+    const cartArray = JSON.parse(localStorage.getItem("cart"));
+    const result = cartArray.findIndex((item) => name == item.name);
+    cartArray[result].quantity++;
+    quantity.text(cartArray[result].quantity);
+
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+})
+
+$(document).on("click", ".decrementCart", function () {
+    const name = $(this).parent().parent().parent().children(".first_row").children("h4").text();
+    console.log(name)
+    const cartArray = JSON.parse(localStorage.getItem("cart"));
+    const result = cartArray.findIndex((item) => name == item.name);
+
+    if(cartArray[result].quantity == 1){
+        let text = "Do you want to remove item from cart";
+        if (confirm(text) == true) {
+            cartArray.splice(result, 1);
+            console.log("success")
+            localStorage.setItem("cart", JSON.stringify(cartArray));
+            $(this).parent().parent().parent().remove();
+        } else {        
+            return false;
+        }
+    }else{
+    cartArray[result].quantity--;
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+    $(this).parent().children("span").text(cartArray[result].quantity);
+    }
+})
+
+// remove item from cart
+$(document).on("click", ".removeCart", function () {
+    const name = $(this).parent().parent().children(".first_row").children("h4").text();
+   console.log(name)
+    const cartArray = JSON.parse(localStorage.getItem("cart"));
+    const result = cartArray.findIndex((item) => name == item.name);
+    cartArray.splice(result, 1);
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+    $(this).parent().parent().remove();
+})
+
+
+
+
+// get cart from local storage and show on cart.html
+var cartItems = JSON.parse(localStorage.getItem("cart"));
+var cartHTML = $.map(cartItems, function (item) {
+   return ( 
+    `<div class="cart-item" key=${item.id}>`+
+      '<div class="first_row" style="display: flex;">'+
+        ` <img src=${item.image} alt="" width="100px"  /> ` +
+         ' <h4>' +
+          item.name
+         +'</h4>' +
+         ' <div style="display: flex; margin-left: auto;"> ' +
+             `<p>${item.price}</p>`+
+        '  </div> ' +
+       '</div>'+
+       '<div class="second_row" style="display: flex; padding: 1em 0;"> ' +
+          '<button class="removeCart">Remove</button>' +
+         ' <div class="" style="margin-left: auto;">'+
+            '<button class="decrementCart">-</button>' +
+             `<span id="item-quantity">${item.quantity}</span>`+
+             '<button class="incrementCart">+</button>' +
+          '</div>' +
+       '</div>' +
+   ' </div>' 
+   );
+});
+$("#cartItems").html(cartHTML);
